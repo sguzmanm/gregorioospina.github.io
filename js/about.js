6e9d486c00d1bfedc8df04c82a1e7df2eabe8738
Sergio Guzmán Mayorga <s.guzmanm98@gmail.com>
2019-08-19 13:03:19 -0500

    Comment regarding var

--- js/about.js
+++ js/about.js
@@ -4,6 +4,7 @@
 // Esta atento a un hover de mouse sobre los elementos de lista para hace run highliht sobre ellos.
 let items = document.getElementsByClassName("list-group-item");
 for (var i = 0; i < items.length; i++) {
+  // sguzmanm: Remember the class, var should not be used for JS ES5 onwards, because in can lead to function scope confusions
   var element = items[i];
   element.addEventListener("mouseenter", event =>{
     event.target.classList.add("active");
